6f14c943a3e98d92e3fd8dded3b58e079034fc97
Wanna <3161434851@qq.com>
2024-06-12 01:12:44 +0800

    message 8:Before refactoring the functional_tests

--- functional_tests.py
+++ functional_tests.py
@@ -36,7 +36,7 @@ class NewVisitorTest(unittest.TestCase):
         # 回车页面更新
         # 待办事项表格显示了"1: Buy flowers"
         inputbox.send_keys(Keys.ENTER)
-        time.sleep(1)
+        time.sleep(2)
         
         table=self.browser.find_element(By.ID,'id_list_table')
         rows=table.find_elements(By.TAG_NAME,'tr')
@@ -44,12 +44,20 @@ class NewVisitorTest(unittest.TestCase):
 
         # 页面又显示了文本框，可以输其他待办事项
         # 输入"Send a gift to Lisi"
-        self.fail('Finish the test!')
+        inputbox=self.browser.find_element(By.ID,'id_new_item')
+        inputbox.send_keys('Give a gift to Lisi')
+        inputbox.send_keys(Keys.ENTER)
+        time.sleep(1)
+
         # 页面再次更新，现在有两个待办事项
+        table=self.browser.find_element(By.ID,'id_new_table')
+        rows=table.find_elements(By.TAG_NAME,'tr')
+        self.assertIn('1:Buy flowers',[row.text for row in rows])
+        self.assertIn('2:Give a gift to Lisi',[row.text for row in rows])
 
         # 张三想知道这网站是否会记住他的清单
         # 网站生成了一个唯一的url
-
+        self.fail('Finish the test!')
         # 访问url，发现待办事项列表还在
 
 if __name__=='__main__':
